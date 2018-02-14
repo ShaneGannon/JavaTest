600d1b093f62d7304efe43acf8ef5c755894ec6a
SGannon98 <31891822+SGannon98@users.noreply.github.com>
2018-02-14 16:29:57 +0000

    fields and accsesors

---
+++ JavaTest.java
@@ -0,0 +1,13 @@
+package ie.dit;
+
+Public class Tune {
+
+	//create the private fields
+	private int x;
+	
+	private String title;
+	private String alttitle;
+	private String notation;
+
+	//create the public accessors
+}
